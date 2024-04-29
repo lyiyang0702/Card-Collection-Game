0fa47091c45b0266e496e60a39489fbc45701fa3
Yiyang Lu <lyiyang@ucsc.edu>
2024-04-28 20:40:58 -0700

    Camera Movement

--- Assets/CameraManger.cs
+++ Assets/CameraManger.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class CameraManger : MonoBehaviour
 {
+    public Camera cam;
+    PlayerController player;
     // Start is called before the first frame update
     void Start()
     {
-        
+        player = PlayerController.Instance;
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+
+        cam.transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -10);
     }
 }
